62320c7a9547dbcc77489f8ff62b8351e5f4c3be
JanithMS <janithms9920@gmail.com>
2021-01-07 21:54:26 +0530

    this is test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+//lot of error
 import 'package:flutter/material.dart';
 import 'package:todo_list_app/new_items_veiw.dart';
 import 'package:todo_list_app/todo.dart';
@@ -30,7 +30,7 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> {
   List<Todo> items = new List<Todo>();
 
   @override
@@ -49,49 +49,48 @@ class HomeState extends State<Home>{
             setState(() {
               goToNewItemView();
             });
-            },
+          },
         ),
-        body: renderBody()
-    );
+        body: renderBody());
   }
 
-  Widget renderBody(){
-    if(items.length > 0){
+  Widget renderBody() {
+    if (items.length > 0) {
       return buildListView();
-    }else{
+    } else {
       return emptyList();
     }
   }
 
-  Widget emptyList(){
-    return Center(
-        child:  Text('No Task')
-    );
+  Widget emptyList() {
+    return Center(child: Text('No Task'));
   }
-
 
   Widget buildListView() {
     return ListView.builder(
       itemCount: items.length,
-      itemBuilder: (BuildContext context,int index){
+      itemBuilder: (BuildContext context, int index) {
         return buildItem(items[index], index);
       },
     );
   }
 
-  Widget buildItem(Todo item, index){
+  Widget buildItem(Todo item, index) {
     return Dismissible(
       key: Key('${item.hashCode}'),
       background: Container(
-          color: Colors.greenAccent[200],
-              child: Row(
-                children: <Widget>[
-                  SizedBox(width: 10,),
-                  Icon(Icons.delete,
-                  color: Colors.white,
-                  ),
-                ],
-              ),
+        color: Colors.greenAccent[200],
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
       onDismissed: (direction) => _removeItemFromList(item),
       direction: DismissDirection.startToEnd,
@@ -99,7 +98,7 @@ class HomeState extends State<Home>{
     );
   }
 
-  Widget buildListTile(item, index){
+  Widget buildListTile(item, index) {
     return ListTile(
       onTap: () => changeItemCompleteness(item),
       onLongPress: () => goToEditItemView(item),
@@ -108,50 +107,48 @@ class HomeState extends State<Home>{
         key: Key('item-$index'),
         style: TextStyle(
             color: item.completed ? Colors.grey : Colors.black,
-            decoration: item.completed ? TextDecoration.lineThrough : null
-        ),
+            decoration: item.completed ? TextDecoration.lineThrough : null),
       ),
-      trailing: Icon(item.completed
-          ? Icons.check_box
-          : Icons.check_box_outline_blank,
+      trailing: Icon(
+        item.completed ? Icons.check_box : Icons.check_box_outline_blank,
         key: Key('completed-icon-$index'),
       ),
     );
   }
 
-  void changeItemCompleteness(Todo item){
+  void changeItemCompleteness(Todo item) {
     setState(() {
       item.completed = !item.completed;
     });
   }
 
-  void goToNewItemView(){
-    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+  void goToNewItemView() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
       return NewTodoView();
-    })).then((title){
-      if(title != null) {
+    })).then((title) {
+      if (title != null) {
         addItem(Todo(title: title));
       }
     });
   }
 
-  void addItem(Todo item){
+  void addItem(Todo item) {
     setState(() {
       items.insert(0, item);
     });
   }
 
-  void goToEditItemView(item){
-    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+  void goToEditItemView(item) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
       return NewTodoView(item: item);
-    })).then((title){
-      if(title != null) {
+    })).then((title) {
+      if (title != null) {
         editItem(item, title);
       }
     });
   }
 
-  void editItem(Todo item ,String title){
+  void editItem(Todo item, String title) {
     item.title = title;
   }
 
@@ -161,7 +158,7 @@ class HomeState extends State<Home>{
     });
   }
 
-  void deleteItem(item){
+  void deleteItem(item) {
     items.remove(item);
   }
 }
